07d0a9ea0da804e3518226200aa2bc57583181ae
Your Name <user@example.com>
2025-04-18 12:35:49 +0500

    Fix API

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -21,11 +21,11 @@ app.get('/', (req, res) => {
 });
 
 // Proxy routes to respective services
-app.use('/menu', createProxyMiddleware({ target: 'http://menu-service:3001', changeOrigin: true }));
-app.use('/orders', createProxyMiddleware({ target: 'http://order-service:3002', changeOrigin: true }));
-app.use('/payments', createProxyMiddleware({ target: 'http://payment-service:3003', changeOrigin: true }));
-app.use('/inventory', createProxyMiddleware({ target: 'http://inventory-service:3004', changeOrigin: true }));
-app.use('/customers', createProxyMiddleware({ target: 'customer-service:3005', changeOrigin: true }));
+app.use('/menu', createProxyMiddleware({ target: 'http://menu-service:3001', changeOrigin: true, pathRewrite: {'^/menu': ''} }));
+app.use('/orders', createProxyMiddleware({ target: 'http://order-service:3002', changeOrigin: true, pathRewrite: {'^/orders': ''} }));
+app.use('/payments', createProxyMiddleware({ target: 'http://payment-service:3003', changeOrigin: true, pathRewrite: {'^/payments': ''} }));
+app.use('/inventory', createProxyMiddleware({ target: 'http://inventory-service:3004', changeOrigin: true, pathRewrite: {'^/inventory': ''} }));
+app.use('/customers', createProxyMiddleware({ target: 'http://customer-service:3005', changeOrigin: true, pathRewrite: {'^/customers': ''} }));
 
 app.listen(port, () => {
   console.log(`API Gateway running on port ${port}`);
